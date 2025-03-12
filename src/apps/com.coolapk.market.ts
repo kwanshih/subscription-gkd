5e6dbc016b743d24f8ac1122da680f89e37215aa
kwan <ellopha@gmail.com>
2025-03-12 14:13:24 +0800

    Create com.coolapk.market.ts
    add coolapk market feed rules

---
+++ src/apps/com.coolapk.market.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.coolapk.market',
+  name: '酷安',
+  groups: [
+    {
+      key: 1,
+      name: '信息流广告',
+      activityIds: ['com.coolapk.market.view.feed.FeedDetailActivityV8'],
+      rules: [
+        {
+          key: 0,
+          name: '评论区信息流广告右上角关闭',
+          matches:'ImageView[id="com.coolapk.market:id/close_view"]',
+        }
+      ],
+    },
+  ],
+});
