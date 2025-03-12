6699a08c41536b62f4213a071b413b8a1fddc8f3
kwan <ellopha@gmail.com>
2025-03-12 18:04:02 +0800

    Update com.coolapk.market.ts

--- src/apps/com.coolapk.market.ts
+++ src/apps/com.coolapk.market.ts
@@ -5,14 +5,19 @@ export default defineGkdApp({
   name: '酷安',
   groups: [
     {
-      key: 1,
+      key: 0,
       name: '信息流广告',
+      desc: '关闭信息流中的卡片广告',
+      enable: false,
+      matchRoot: true,
       activityIds: ['com.coolapk.market.view.feed.FeedDetailActivityV8'],
+      fastQuery: true,
       rules: [
         {
-          key: 0,
+          key: 1,
           name: '评论区信息流广告右上角关闭',
-          matches: 'ImageView[id="com.coolapk.market:id/close_view"]',
+          matches: 'ImageView[vid="close_view"]',
+          snapshotUrls：''
         },
       ],
     },
