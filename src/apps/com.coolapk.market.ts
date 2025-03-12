7f2c6d992e5580fe57c2ad93e74eb90b6325189e
kwan <ellopha@gmail.com>
2025-03-12 18:55:15 +0800

    Update com.coolapk.market.ts

--- src/apps/com.coolapk.market.ts
+++ src/apps/com.coolapk.market.ts
@@ -10,12 +10,12 @@ export default defineGkdApp({
       desc: '关闭信息流中的卡片广告',
       enable: false,
       matchRoot: true,
-      activityIds: ['com.coolapk.market.view.feed.FeedDetailActivityV8'],
+      activityIds: ['com.coolapk.market.view.feed.FeedDetailActivityV8', 'com.coolapk.market.view.main.MainActivity'],
       fastQuery: true,
       rules: [
         {
           key: 1,
-          name: '评论区信息流广告右上角关闭',
+          name: '评论区广告|信息流广告',
           matches: 'ImageView[vid="close_view"]',
         },
       ],
